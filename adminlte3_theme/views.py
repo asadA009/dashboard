3eb58e5292518533ad453a124c6175acbc0dab44
asadA009 <asadali4345425@gmail.com>
2021-02-23 14:54:01 +0500

    first commit

--- adminlte3_theme/views.py
+++ adminlte3_theme/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import serializers
+from .models import confrence
+from .seriliazers import confrenceSerializer
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+#from rest_framework import viewsets
+
+def conf_list(request):
+    stu=confrence.objects.all()
+    print(stu)
+    #stu=confrence.objects.all()
+    serializer=confrenceSerializer(stu,many=True)
+    json_data=JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
